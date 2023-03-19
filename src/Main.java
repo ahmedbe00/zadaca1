bbc634ab86db48dcca89373a9f24436de8843a65
Ahmed Berbic <ahmedberbic2000@gmail.com>
2023-03-19 20:52:52 +0100

    First version of homework no.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,131 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//  Task 4
+
+        /*
+      int BirthYear = 2000 ;
+        String Name = BirthYear ;
+
+        String Prezime = "Berbic" ;
+        int  Years = Prezime ;
+      */
+
+// Task 5
+
+        String Name = "Ahmed";
+        String Prezime = "Berbic";
+        char FirstLetterName = 'A';
+        int YearOfBirth = 2000;
+        // int Number = 100000000000 ;  *Number too big, must use long* //
+        Long Number = 100000000000L;
+        double DecimalNumber = 42.001;
+
+// Task 6
+
+        char letter1 = 65;
+        char letter2 = 104;
+        char letter3 = 109;
+        char letter4 = 101;
+        char letter5 = 100;
+        System.out.println(letter1 + "" + letter2 + "" + letter3 + "" + letter4 + "" + letter5);
+       /*
+                OR THIS
+        System.out.print (letter1);
+        System.out.print (letter2);
+        System.out.print (letter3);
+        System.out.print (letter4);
+        System.out.print (letter5); */
+
+// Task 7
+
+        boolean PositiveAnswerQuestion = true;
+        boolean NegativeAnswerQuestion = false;
+        System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42: " + PositiveAnswerQuestion);
+        System.out.println("Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42: " + NegativeAnswerQuestion);
+        System.out.println();
+
+// Task 7
+
+       // Version 1
+
+        /* int  NumberToDouble = 1 ;
+        System.out.println("This is a number " + NumberToDouble + " and it is about to double; " );
+
+        int DoubledNumber = NumberToDouble*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; " );
+        int DoubledNumber2 = DoubledNumber*2 ;
+        System.out.println("This is a number " + DoubledNumber2 + " and it is about to double; " );
+        int DoubledNumber3 = DoubledNumber2*2 ;
+        System.out.println("This is a number " + DoubledNumber3 + " and it is about to double; " );
+        int DoubledNumber4 = DoubledNumber3*2 ;
+        System.out.println("This is a number " + DoubledNumber4 + " and it is about to double; " );
+        int DoubledNumber5 = DoubledNumber4*2 ;
+        System.out.println("This is a number " + DoubledNumber5 + " and it is about to double; " );
+        int DoubledNumber6 = DoubledNumber5*2 ;
+        System.out.println("This is a number " + DoubledNumber6 + " and it is about to double; " );
+        int DoubledNumber7 = DoubledNumber6*2 ;
+        System.out.println("This is a number " + DoubledNumber7 + " and it is about to double; " );
+        int DoubledNumber8 = DoubledNumber7*2 ;
+        System.out.println("This is a number " + DoubledNumber8 + " and it is about to double; " ); */
+
+        // Version 2 with BONUS task
+
+        int NumberToDouble = 5;
+        int DoubledNumber = NumberToDouble * 2;
+        int NumberOfDoubling = 1 ;
+
+        System.out.println("This is a number " + NumberToDouble + " and it is about to double; ");
+        System.out.println();
+
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+
+        DoubledNumber = DoubledNumber * 2;
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+        DoubledNumber = DoubledNumber * 2;
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+        DoubledNumber = DoubledNumber * 2;
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+        DoubledNumber = DoubledNumber * 2;
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+        DoubledNumber = DoubledNumber * 2;
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+        DoubledNumber = DoubledNumber * 2;
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+        DoubledNumber = DoubledNumber * 2;
+        NumberOfDoubling = NumberOfDoubling*2 ;
+        System.out.println("This is a number " + DoubledNumber + " and it is about to double; ");
+        System.out.println("The original number has been doubled: " + NumberOfDoubling + " times. ");
+        System.out.println();
+
+
     }
 }
